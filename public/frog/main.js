0c2e9e2159e885103d7e7bb858cecbb21f317251
Cameron Rye <c@meron.io>
2017-04-12 13:23:13 -0400

    Initial

---
+++ public/frog/main.js
@@ -0,0 +1,16 @@
+let frog = {};
+
+(function () {
+  'use strict'
+  
+  frog.init = {
+    bootstrap: function bootstrap () {
+    }
+  }
+  document.addEventListener('DOMContentLoaded', function () {
+    frog.init.bootstrap()
+  })
+  window.onresize = function() {
+
+  }
+})()
